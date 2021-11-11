40b0d7173a0cb8d1a2b0a3c83055ee1b5b40f514
lionelvoirol <lionelvoirol@hotmail.com>
2021-11-11 21:07:23 +0100

    more data

--- scripts/save_data_in_RData.R
+++ scripts/save_data_in_RData.R
@@ -1,5 +1,22 @@
 # save file in data as .RData file
 
-diet <- read_csv("data_raw/diet.csv")
+bronchitis <- read_csv("data-raw/bronchitis.csv")
+save(bronchitis, file = "data/bronchitis.RData")
+
+cortisol <- read_csv("data-raw/cortisol.csv")
+save(cortisol, file = "data/cortisol.RData")
+
+covid <- read_csv("data-raw/covid.csv")
+save(covid, file = "data/covid.RData")
+
+diet <- read_csv("data-raw/diet.csv")
 save(diet, file = "data/diet.RData")
 
+pharmacy <- read_csv("data-raw/pharmacy.csv")
+save(pharmacy, file = "data/pharmacy.RData")
+
+reading <- read_csv("data-raw/reading.csv")
+save(reading, file = "data/reading.RData")
+
+students <- read_csv("data-raw/students.csv")
+save(students, file = "data/students.RData")
